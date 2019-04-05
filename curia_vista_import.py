33e173dd0a8f09f9974db57bd129b1bbed5c4289
Reto Schneider <code@reto-schneider.ch>
2019-04-05 03:09:10 +0200

    WIP

--- curia_vista_import.py
+++ curia_vista_import.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import argparse
+from json import JSONDecodeError
 
-from curia_vista import create_parser
+import requests
+
+from curia_vista import create_parser, logger, _to_snake_case
 
 URL = 'https://ws.parlament.ch/odata.svc/'
 
@@ -14,10 +17,25 @@ def main():
     xml = "".join(args.schema.readlines())
     parser = create_parser(xml)
 
-    for e in parser.entities:
-        print(e.table_name)
+    for entity in parser.entities:
+        url = "{}{}?$select={}".format(URL, entity.name, ",".join(p.name for p in entity.properties))
+        r = requests.get(url, headers={'Accept': 'application/json'})
+        logger.info("HTTP code {} for url {}".format(r.status_code, url))
+        try:
+            j = r.json()
+        except JSONDecodeError as e:
+            logger.error("Failed to decode JSON message: {}".format(e))
+            return
+        for result in j['d']['results']:
+            column_names = []
+            values = []
+            for name, value in result.items():
+                if isinstance(value, dict):
+                    continue
+                column_names.append(_to_snake_case(name))
+                values.append(value)
 
-    # parsed = json.loads(fetch(url))
+        # command = 'INSERT OR REPLACE INTO %s VALUES (%s)' % (us(table), ', '.join(['?'] * len(column_names)))
 
 
 if __name__ == '__main__':
