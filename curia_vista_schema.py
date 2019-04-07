a03c0051ac91be908f5975fc8abc82576459c854
Reto Schneider <code@reto-schneider.ch>
2019-04-07 23:48:49 +0200

    curia_vista_schema.py: Argument --schema is required

--- curia_vista_schema.py
+++ curia_vista_schema.py
@@ -6,7 +6,7 @@ from curia_vista import schema_to_sql
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--schema", type=argparse.FileType('r'))
+    parser.add_argument("--schema", type=argparse.FileType('r'), required=True)
     args = parser.parse_args()
 
     xml = "".join(args.schema.readlines())
