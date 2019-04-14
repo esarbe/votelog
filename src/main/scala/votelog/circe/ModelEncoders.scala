f0e33731e4aeac0e503c7e260cc3ebeedd55062f
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-04-14 22:49:29 +0200

    make id encoders match the id decoders

--- src/main/scala/votelog/circe/ModelEncoders.scala
+++ src/main/scala/votelog/circe/ModelEncoders.scala
@@ -1,13 +1,14 @@
 package votelog.circe
 
 import io.circe
+import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
 import votelog.domain.model.{Motion, Politician, Votum}
 
 trait ModelEncoders {
-  implicit val politicianIdEncoder: circe.Encoder[Politician.Id] = deriveEncoder[Politician.Id]
+  implicit val politicianIdCirceEncoder: circe.Encoder[Politician.Id] = Encoder.encodeLong.contramap(_.value)
   implicit val politicianCirceEncoder: circe.Encoder[Politician] = deriveEncoder[Politician]
-  implicit val motionIdCirceEncoder: circe.Encoder[Motion.Id] = deriveEncoder[Motion.Id]
+  implicit val motionIdCirceEncoder: circe.Encoder[Motion.Id] = Encoder.encodeLong.contramap(_.value)
   implicit val motionCirceEncoder: circe.Encoder[Motion] = deriveEncoder[Motion]
   implicit val votumCirceEncoder: circe.Encoder[Votum] = deriveEncoder[Votum]
 }
