25671fc31a105cec4db472159a0fa86e77533932
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-05-19 00:53:58 +0200

    generate factory only once at instance creation

--- src/main/scala/votelog/crypto/PasswordHasherJavaxCrypto.scala
+++ src/main/scala/votelog/crypto/PasswordHasherJavaxCrypto.scala
@@ -1,21 +1,22 @@
 package votelog.crypto
 
 import cats.effect.Sync
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
 import votelog.crypto.PasswordHasherJavaxCrypto.Salt
 
 class PasswordHasherJavaxCrypto[F[_]: Sync](
   salt: Salt,
 ) extends PasswordHasherAlg[F] {
+
+  val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+
   def hashPassword(password: String): F[String] =
     Sync[F].delay {
-      import javax.crypto.SecretKeyFactory
       import javax.crypto.spec.PBEKeySpec
       import java.util.Base64
 
       val spec = new PBEKeySpec(password.toCharArray, salt.value.getBytes("UTF-8"), 65536,  128)
-
-      val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
-
       Base64.getEncoder.encodeToString(factory.generateSecret(spec).getEncoded)
     }
 }
