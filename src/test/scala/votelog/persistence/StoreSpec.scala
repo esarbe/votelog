9e98ab100c40ca9604bdbb5ca71845207d9b9e0b
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-05-23 22:29:10 +0200

    make StoreSpec shareabe

---
+++ src/test/scala/votelog/persistence/StoreSpec.scala
@@ -0,0 +1,72 @@
+package votelog.persistence
+
+import cats.effect.IO
+import org.scalatest.{FlatSpec, Inside, Matchers, WordSpec}
+import votelog.infrastructure.StoreAlg
+
+trait StoreSpec
+  extends FlatSpec
+    with Matchers
+    with Inside {
+
+  def aRegualStore[Entity, Id, Recipe](
+    store: StoreAlg[IO, Entity, Id, Recipe],
+    creationRecipe: Recipe,
+    createdEntity: Id => Entity,
+    updatedRecipe: Recipe,
+    updatedEntity: Id => Entity,
+  ){
+
+    it should "be able to store an entity" in {
+      val check =
+        for {
+          indexBefore <- store.index
+          _ <- store.create(creationRecipe)
+          indexAfter <- store.index
+        } yield {
+          indexBefore shouldBe empty
+          indexAfter.length shouldBe 1
+        }
+
+      check.unsafeRunSync()
+    }
+
+
+    it should "be able to read a stored entity" in {
+      val entities = store.index.unsafeRunSync()
+
+      inside(entities) { case List(id) =>
+        val entity = store.read(id).unsafeRunSync()
+
+        entity shouldBe createdEntity(id)
+      }
+    }
+
+
+    it should "be able to update stored entity" in {
+      val entities = store.index.unsafeRunSync()
+      inside(entities) { case List(id) =>
+        store.update(id, updatedRecipe).unsafeRunSync()
+
+        val entity = store.read(id).unsafeRunSync()
+
+        entity shouldBe updatedEntity(id)
+      }
+    }
+
+
+    it should "be able to delete stored entity" in {
+      val entities = store.index.unsafeRunSync()
+
+      inside(entities) { case List(id) =>
+        val check =
+          for {
+            _ <- store.delete(id)
+            entities <- store.index
+          } yield entities shouldBe empty
+
+        check.unsafeRunSync()
+      }
+    }
+  }
+}
--- src/test/scala/votelog/persistence/doobie/DoobieMotionStoreSpec.scala
+++ src/test/scala/votelog/persistence/doobie/DoobieMotionStoreSpec.scala
@@ -4,13 +4,19 @@ package votelog.persistence.doobie
 import cats.effect.IO
 import doobie.util.transactor.Transactor
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{Inside, Matchers, WordSpec}
+import org.scalatest.{FlatSpec, Inside, Matchers, WordSpec}
+import votelog.domain.politics.Motion
 import votelog.persistence.MotionStore.Recipe
-import votelog.persistence.{MotionStore, PoliticianStore}
+import votelog.persistence.{MotionStore, PoliticianStore, StoreSpec}
 
 import scala.concurrent.ExecutionContext
 
-class DoobieMotionStoreSpec extends WordSpec with ScalaFutures with Matchers with Inside {
+class DoobieMotionStoreSpec
+  extends FlatSpec
+    with StoreSpec
+    with ScalaFutures
+    with Matchers
+    with Inside {
 
   implicit val cs = IO.contextShift(ExecutionContext.global)
   implicit val transactor =
@@ -27,62 +33,13 @@ class DoobieMotionStoreSpec extends WordSpec with ScalaFutures with Matchers wit
 
   schema.initialize.unsafeRunSync()
 
-  "DoobieMotionStore" should {
-    val pid1 = politician.create(PoliticianStore.Recipe("foo")).unsafeRunSync()
-    val pid2 = politician.create(PoliticianStore.Recipe("bar")).unsafeRunSync()
+  val pid1 = politician.create(PoliticianStore.Recipe("foo")).unsafeRunSync()
+  val pid2 = politician.create(PoliticianStore.Recipe("bar")).unsafeRunSync()
 
-    val recipe = MotionStore.Recipe("foo-motion", pid1)
+  val creationRecipe: Recipe = MotionStore.Recipe("foo-motion", pid1)
+  val createdEntity: Motion.Id => Motion = Motion(_, "foo-motion", pid1)
+  val updatedRecipe: Recipe = Recipe("updated-name", pid2)
+  val updatedEntity: Motion.Id => Motion = Motion(_, "updated-name", pid2)
 
-    "be able to store an entity" in {
-      val check =
-        for {
-          indexBefore <- store.index
-          _ <- store.create(recipe)
-          indexAfter <- store.index
-        } yield {
-          indexBefore shouldBe empty
-          indexAfter.length shouldBe 1
-        }
-
-      check.unsafeRunSync()
-    }
-
-
-    "be able to read a stored entity" in {
-      val entities = store.index.unsafeRunSync()
-
-      inside(entities) { case List(id) =>
-        val entity = store.read(id).unsafeRunSync()
-        entity.submitter shouldBe recipe.submitter
-        entity.name shouldBe recipe.name
-      }
-    }
-
-    "be able to update stored entity" in {
-      val entities = store.index.unsafeRunSync()
-      inside(entities) { case List(id) =>
-        store.update(id, Recipe("updated-name", pid2)).unsafeRunSync()
-
-        val updatedEntity = store.read(id).unsafeRunSync()
-
-        updatedEntity.name shouldBe "updated-name"
-        updatedEntity.submitter shouldBe pid2
-      }
-    }
-
-
-    "be able to delete stored entity" in {
-      val entities = store.index.unsafeRunSync()
-
-      inside(entities) { case List(id) =>
-        val check =
-          for {
-            _ <- store.delete(id)
-            entities <- store.index
-          } yield entities shouldBe empty
-
-        check.unsafeRunSync()
-      }
-    }
-  }
+  it should behave like aRegualStore(store, creationRecipe, createdEntity, updatedRecipe, updatedEntity)
 }
