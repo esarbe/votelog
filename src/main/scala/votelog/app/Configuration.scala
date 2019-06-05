82037bbff9cf1f8264301656e65feb6b0f2ca6e2
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-06-06 01:00:17 +0200

    create new driver config so we could use different databases

--- src/main/scala/votelog/app/Configuration.scala
+++ src/main/scala/votelog/app/Configuration.scala
@@ -9,5 +9,14 @@ case class Configuration(
 object Configuration {
   case class Http(port: Int, interface: String)
   case class Security(passwordSalt: String, secret: String)
-  case class Database(url: String, user: String, password: String)
+  case class Database(driver: Database.Driver, url: String, user: String, password: String)
+
+  object Database {
+    sealed trait Driver { val name: String }
+
+    object Driver {
+      case object H2 extends Driver { val name: String = "org.h2.Driver"}
+      case object Postgres extends Driver { val name: String = "org.postgresql.Driver"}
+    }
+  }
 }
