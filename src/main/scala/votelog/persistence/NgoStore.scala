a023a123ebafe63b586513afbe4499d55e284114
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-06-17 17:49:56 +0200

    implement scoring

--- src/main/scala/votelog/persistence/NgoStore.scala
+++ src/main/scala/votelog/persistence/NgoStore.scala
@@ -2,11 +2,11 @@ package votelog.persistence
 
 import java.util.UUID
 
-import votelog.domain.politics.Ngo
+import votelog.domain.politics.{Ngo, Scoring}
 import votelog.infrastructure.StoreAlg
 import votelog.persistence.NgoStore.Recipe
 
-trait NgoStore[F[_]] extends StoreAlg[F, Ngo, Ngo.Id, Recipe]
+trait NgoStore[F[_]] extends StoreAlg[F, Ngo, Ngo.Id, Recipe] with Scoring[F]
 
 object NgoStore {
 
