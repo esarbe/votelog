ddfdcf9f527a0776a7a8475bf71962301b797850
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-06-18 21:18:53 +0200

    cleanup scoring

--- src/main/scala/votelog/domain/politics/Scoring.scala
+++ src/main/scala/votelog/domain/politics/Scoring.scala
@@ -3,9 +3,8 @@ package votelog.domain.politics
 import votelog.domain.politics.Scoring.Score
 
 trait Scoring[F[_]] {
-  def politiciansScoredBy(ngo: Ngo.Id): F[List[(Politician.Id, Score)]]
   def motionsScoredBy(ngo: Ngo.Id): F[List[(Motion.Id, Score)]]
-  def scorePolitician(ngo: Ngo.Id, politician: Politician.Id, score: Score): F[Unit]
+  def politiciansScoredBy(ngo: Ngo.Id): F[List[(Motion.Id, Score)]]
   def scoreMotion(ngo: Ngo.Id, motion: Motion.Id, score: Score): F[Unit]
 }
 
