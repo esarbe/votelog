cc45ce31fea4822c2394f0619cd77df8891444a1
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-11-14 14:08:37 +0100

    add deployment for pages

---
+++ deployment/deploy.sh
@@ -0,0 +1,19 @@
+if [ ! -d "public" ]; then
+	mkdir public
+fi
+
+if [ ! -d "public/css" ]; then
+	mkdir public/css
+fi
+
+if [ ! -d "public/js" ]; then
+	mkdir public/js
+fi
+
+
+# Generate .js files from Scala
+sbt fastOptJS
+
+cp webclient/src/main/resources/index.html public
+cp webclient/src/main/resources/css/* public/css/
+cp webclient/target/scala-*/*.js public/js/
