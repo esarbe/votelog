27f6af031ed8795204145cf9d21bfa584d54446c
root <root@mithlond.localdomain>
2019-11-14 13:26:24 +0100

    unbreak test

--- webserver/src/test/scala/votelog/domain/authorization/ComponentSpec.scala
+++ webserver/src/test/scala/votelog/domain/authorization/ComponentSpec.scala
@@ -7,22 +7,22 @@ class ComponentSpec extends WordSpec with Matchers {
   "Component" should {
 
     "contains" in {
-      Component("a.b.c.").contains(Component("c")) shouldBe false
-      Component("c").contains(Component("a.b.c")) shouldBe false
-      Component("a").contains(Component("a.b.c")) shouldBe true
-      Component("a.b.c").contains(Component("a.b.c.d")) shouldBe true
-      Component("ab.c").contains(Component("a")) shouldBe false
-      Component("a.b").contains(Component("a.c.b")) shouldBe false
-      Component("a.b").contains(Component("a.b.c.d")) shouldBe true
-      Component(".").contains(Component(".a")) shouldBe true
-      Component(".").contains(Component(".foo.bar")) shouldBe true
-      Component("").contains(Component(".foo.bar")) shouldBe true
+      Component("a/b/c/").contains(Component("c")) shouldBe false
+      Component("c").contains(Component("a/b/c")) shouldBe false
+      Component("a").contains(Component("a/b/c")) shouldBe true
+      Component("a/b/c").contains(Component("a/b/c/d")) shouldBe true
+      Component("ab/c").contains(Component("a")) shouldBe false
+      Component("a/b").contains(Component("a/c/b")) shouldBe false
+      Component("a/b").contains(Component("a/b/c/d")) shouldBe true
+      Component("/").contains(Component("/a")) shouldBe true
+      Component("/").contains(Component("/foo/bar")) shouldBe true
+      Component("").contains(Component("/foo/bar")) shouldBe true
     }
 
     "name" in {
-      Component("a.b.c").name shouldBe "c"
+      Component("a/b/c").name shouldBe "c"
       Component("").name shouldBe ""
-      Component(".").name shouldBe ""
+      Component("/").name shouldBe ""
     }
   }
 
