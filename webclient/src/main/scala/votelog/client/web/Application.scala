fbbea96d08dc50765e8066c6696f7e46b26c3f1e
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-11-21 21:33:44 +0100

    render languages

--- webclient/src/main/scala/votelog/client/web/Application.scala
+++ webclient/src/main/scala/votelog/client/web/Application.scala
@@ -4,13 +4,14 @@ package web
 import mhtml._
 import mhtml.future.syntax._
 import org.scalajs.dom
+import cats.implicits._
 import org.scalajs.dom.raw.Element
 import votelog.domain.authentication.User
 import votelog.domain.crudi.ReadOnlyStoreAlg.{IndexQueryParameters, QueryParameters}
 import votelog.domain.crudi.ReadOnlyStoreAlg.QueryParameters.{Offset, PageSize}
 import votelog.domain.politics
 import votelog.domain.politics.Person
-import votelog.endpoint.client.{PersonReadOnlyStoreService, PersonStoreXhrEndpoint}
+import votelog.endpoint.client.{PersonReadOnlyStoreAjaxService, PersonStoreXhrEndpoint}
 
 import scala.scalajs.js
 import scala.scalajs.js.timers.SetTimeoutHandle
@@ -29,36 +30,49 @@ object State {
 object Application {
 
   val context = Context("http://localhost:8080/api/v0", politics.Context(2019, politics.Language.English))
+  val path = Var("")
 
-  val personComponent = new PersonReadOnlyStoreService(context.url, context.context)
+  val personComponent = new PersonReadOnlyStoreAjaxService(context.url, context.context)
   val authService = new service.SessionServiceRest(context)
   val authComponent = new components.Authentication(authService)
 
-  def main(args: Array[String]): Unit = {
+  val languageComponent = new components.Language
 
-    val indexQueryParams = IndexQueryParameters(PageSize(100), Offset(0), QueryParameters(context.context.language.iso639_1))
+  def main(args: Array[String]): Unit = {
+    val qp = QueryParameters(context.context.language, 2019)
+
+    val indexQueryParams = IndexQueryParameters(PageSize(100), Offset(0), qp)
     val personIndex: Rx[Seq[Person.Id]] =
-        authComponent.model
-        .flatMap { _ =>
-          personComponent
-            .index(indexQueryParams)
-            .toRx
-            .collect { case Some(Success(persons)) => persons }(Nil)
-      }
+      personComponent
+        .index(indexQueryParams)
+        .toRx
+        .collect { case Some(Success(persons)) => persons }(Nil)
+
 
     val pview: Node =
-      <div>
+      <section>
+        <header>Users</header>
         <ul>
-          {personIndex.map { id => <li>id</li> }}
+          { personIndex.map { ids => ids.map { id => <li>{id.value}</li> }} }
         </ul>
-      </div>
-
+      </section>
 
     val auth: Element = dom.document.createElement("div")
     val persons: Element = dom.document.createElement("div")
+    val language: Element = dom.document.createElement("div")
+    val languageR: Element = dom.document.createElement("div")
+
     dom.document.body.appendChild(auth)
+    dom.document.body.appendChild(language)
+    dom.document.body.appendChild(persons)
+    dom.document.body.appendChild(languageR)
+
+    val n = <div>{languageComponent.model.map(_.show)}</div>
+
     mount(auth, authComponent.view)
+    mount(language, languageComponent.view)
     mount(persons, pview)
+    mount(languageR, n)
     println("ready.")
   }
 
