eb5cde3413c7088d0e3f1685f64e74f7b73331e7
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-11-21 21:33:32 +0100

    add show to language

--- core/src/main/scala/votelog/domain/politics/Context.scala
+++ core/src/main/scala/votelog/domain/politics/Context.scala
@@ -1,12 +1,26 @@
 package votelog.domain.politics
 
+import cats.Show
+
 case class Context(year: Int, language: Language)
 
-sealed trait Language { def iso639_1: String }
+sealed trait Language extends Product with Serializable { def iso639_1: String }
 object Language {
   case object German extends Language { def iso639_1 = "de" }
   case object English extends Language { def iso639_1 = "en" }
   case object French extends Language { def iso639_1 = "fr" }
   case object Italian extends Language { def iso639_1 = "it" }
   case object Romansh extends Language { def iso639_1 = "rm" }
+
+  val values: Set[Language] = Set(German, English, French, Italian, Romansh)
+
+
+  implicit val languageShow: Show[Language] = {
+    case German => "Deutsch"
+    case English => "English"
+    case French =>  "Francais"
+    case Italian => "Italiano"
+    case Romansh => "Rumansh"
+  }
+
 }
