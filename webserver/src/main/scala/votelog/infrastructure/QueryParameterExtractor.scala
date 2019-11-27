24ca2218fadb84fa506e6bbd803feffb9bc9244b
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-11-27 13:41:05 +0100

    formatting

--- webserver/src/main/scala/votelog/infrastructure/QueryParameterExtractor.scala
+++ webserver/src/main/scala/votelog/infrastructure/QueryParameterExtractor.scala
@@ -1,6 +1,5 @@
 package votelog.infrastructure
 
 abstract class QueryParameterExtractor[T](implicit ev: Param[T]) {
-  def unapply(params: Map[String, Iterable[String]]): Option[T] =
-    ev.decode(params)
+  def unapply(params: Map[String, Iterable[String]]): Option[T] = ev.decode(params)
 }
