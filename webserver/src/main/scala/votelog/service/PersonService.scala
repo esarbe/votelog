2c70473c57a74bbc4d141f4a96169d18fa6de154
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-12-26 00:38:59 +0100

    provide default idqp if non given

--- webserver/src/main/scala/votelog/service/PersonService.scala
+++ webserver/src/main/scala/votelog/service/PersonService.scala
@@ -11,7 +11,9 @@ import org.http4s.dsl.io._
 import votelog.domain.authentication.User
 import votelog.domain.authorization.{AuthorizationAlg, Component}
 import votelog.domain.crudi.ReadOnlyStoreAlg
-import votelog.domain.politics.{Context, Language, Motion, Person}
+import votelog.domain.crudi.ReadOnlyStoreAlg.IndexQueryParameters
+import votelog.domain.crudi.ReadOnlyStoreAlg.QueryParameters.{Offset, PageSize}
+import votelog.domain.politics.{Context, Language, LegislativePeriod, Motion, Person}
 import votelog.infrastructure.logging.Logger
 import votelog.infrastructure.{Param, ReadOnlyStoreService, VoteAlg}
 import votelog.orphans.circe.implicits._
@@ -27,6 +29,9 @@ class PersonService(
 
   implicit val motionIdCirceKeyEncoder: KeyEncoder[Motion.Id] =
     KeyEncoder.instance[Motion.Id](_.value.toString)
+
+  val defaultIndexQueryParameters: IndexQueryParameters[Context] =
+    IndexQueryParameters[Context](PageSize(20), Offset(0), Context(LegislativePeriod.Id(50), Language.English))
 
   object MotionId {
     def unapply(str: String): Option[Motion.Id] =
@@ -45,6 +50,7 @@ class PersonService(
   override def service: AuthedRoutes[User, IO] = super.service <+> voting
 
   override implicit val queryParamDecoder: Param[Language] = Params.languageParam
-  override implicit val indexQueryParamDecoder: Param[ReadOnlyStoreAlg.IndexQueryParameters[Context]] =
-    Params.indexQueryParam(Params.contextParam)
+  override implicit val indexQueryParamDecoder: Param[IndexQueryParameters[Context]] =
+    iqpc => Params.indexQueryParam(Params.contextParam).decode(iqpc)
+      .orElse(Some(defaultIndexQueryParameters)) // TODO: redirect in case of missing required parameters would be better
 }
