aaa26171ddaf54758bcbd0f53b34f1059b7f6215
Raphael Bosshard <raphael.bosshard@gmail.com>
2019-12-26 00:37:48 +0100

    setup form

--- webclient/src/main/scala/votelog/client/web/components/UserComponent.scala
+++ webclient/src/main/scala/votelog/client/web/components/UserComponent.scala
@@ -6,7 +6,7 @@ import cats.implicits._
 import mhtml.future.syntax._
 import mhtml.implicits.cats._
 import mhtml.{Rx, Var}
-import votelog.client.web.components.html.tools.{inputPassword, inputText, set}
+import votelog.client.web.components.html.tools.{ifEnter, inputPassword, inputText, set}
 import votelog.domain.authentication.User
 import votelog.domain.authentication.User.Permission
 import votelog.domain.crudi.StoreAlg
@@ -24,6 +24,7 @@ class UserComponent(
   store: StoreAlg[Future, User, User.Id, Recipe],
   crud: CrudIndexComponent[User, User.Id, UserStore.Recipe]
 ) {
+
   def id(id: String): String = component.child(id).location
 
   object create {
@@ -34,22 +35,25 @@ class UserComponent(
     private val email = Var("")
     private val password = Var("")
     private val confirmPassword = Var("")
-    private val submit = Var(Unit)
+    private val submit: Var[Unit] = Var(())
 
     private val validatedRecipe: Rx[Validated[NonEmptyList[Error], Recipe]] =
-      (name: Rx[String], email: Rx[String], password: Rx[String], confirmPassword: Rx[String])
-        .mapN { case (name, email, password, confirmPassword) =>
-          UserStore.validateRecipe(name, User.Email(email), Password.Clear(password), Password.Clear(confirmPassword))
-        }
+      for {
+        name <- name
+        email <- email
+        password <- password
+        confirmPassword <- confirmPassword
+      } yield UserStore.validateRecipe(name, User.Email(email), Password.Clear(password), Password.Clear(confirmPassword))
 
     private val errors: Rx[List[(String, String)]] =
       validatedRecipe
-        .collect {
+        .map {
           case Invalid(errors) => errors.toList
-        }(Nil)
+          case _ => Nil
+        }
 
     val model: Rx[Option[User.Id]] =
-      validatedRecipe
+      validatedRecipe.sampleOn(submit)
         .flatMap {
           case Valid(recipe) => store.create(recipe).toRx
           case _ => Rx(None)
@@ -59,16 +63,22 @@ class UserComponent(
         case Some(Failure(error)) => println(error.getMessage); None
       }
 
-    def form(legend: String) = {
+    def id(id: String): String = component.child("create").child(id).location
+
+    def form(legend: String): Elem = {
         <article class="user">
-          <fieldset>
+          <fieldset onkeyup={ ifEnter(set(submit)) }>
             <legend>{legend}</legend>
-            { inputText(id = "name", label = "Name", rx = name, errors = errors) }
-            { inputText(id = "email", label = "Email", rx = email, errors = errors) }
-            { inputPassword(id = "password", label = "Password", rx = password, errors = errors) }
-            { inputPassword(id = "confirmPassword", label = "Confirm password", rx = confirmPassword, errors = errors) }
+            { inputText(id = id("name"), label = "Name", rx = name, errors = errors) }
+            { inputText(id = id("email"), label = "Email", rx = email, errors = errors) }
+            { inputPassword(id = id("password"), label = "Password", rx = password, errors = errors) }
+            { inputPassword(id = id("confirmPassword"), label = "Confirm password", rx = confirmPassword, errors = errors) }
 
             <input type="button" value="submit" onclick={ set(submit) } />
+            { errors.map { _.map { case (key, message) =>
+              <error> {key}: {message} </error>
+                }
+            }}
           </fieldset>
         </article>
     }
