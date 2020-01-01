78ff81f048370724faa8e782c53a53e532f27503
Raphael Bosshard <raphael.bosshard@gmail.com>
2020-01-01 16:15:08 +0100

    add fonts to deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -11,8 +11,14 @@ if [ ! -d "public/js" ]; then
 	mkdir public/js
 fi
 
+if [ ! -d "public/fonts" ]; then
+	mkdir public/fonts
+fi
+
+
 sbt fastOptJS
 
 cp webclient/src/main/resources/index.html public/
 cp webclient/src/main/resources/css/* public/css/
+cp webclient/src/main/resources/fonts/* public/fonts/
 cp webclient/target/scala-*/*.js* public/js/
