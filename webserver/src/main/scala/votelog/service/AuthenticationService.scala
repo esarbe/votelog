f388f36b139fa4a11f7c4d02546220e45719f06a
Raphael Bosshard <raphael.bosshard@gmail.com>
2020-03-05 22:04:24 +0100

    add fallback case for authentication

--- webserver/src/main/scala/votelog/service/AuthenticationService.scala
+++ webserver/src/main/scala/votelog/service/AuthenticationService.scala
@@ -10,30 +10,58 @@ import org.http4s.{AuthedRequest, AuthedRoutes, AuthedService, Request, Response
 import org.reactormonk.CryptoBits
 import votelog.domain.authentication.User
 import votelog.persistence.UserStore
+import votelog.service.AuthenticationService.{Cookie, Error, FallbackUsername}
+import votelog.service.AuthenticationService.Error.{CookieInvalid, NoAuthCookie, NoAuthHeader, UnknownUser}
 
 class AuthenticationService(
   userStore: UserStore[IO],
   crypto: CryptoBits,
 ) {
 
-  val cookie = "authcookie"
+  val retrieveUser: Kleisli[IO, String, Either[Error, User]] =
+    Kleisli(name => userStore.findByName(name).map(_.toRight(UnknownUser(name))))
 
-  val retrieveUser: Kleisli[IO, String, Either[String, User]] =
-    Kleisli(userStore.findByName(_).map(_.toRight("unknown username")))
-
-  val authUser: Kleisli[IO, Request[IO], Either[String, User]] = Kleisli({ request =>
-    val maybeUsername: Either[String, String] = for {
-      header <- headers.Cookie.from(request.headers).toRight(s"Could not parse cookies header")
-      cookie <- header.values.toList.find(_.name == cookie).toRight(s"No authentication cookie '$cookie' found")
-      token <- crypto.validateSignedToken(cookie.content).toRight("Cookie invalid")
-      username <- Either.catchOnly[NumberFormatException](token).leftMap(_.getMessage)
+  val authUser: Kleisli[IO, Request[IO], Either[Error, User]] = Kleisli({ request =>
+    val maybeUsername: Either[Error, String] = for {
+      header <- headers.Cookie.from(request.headers).toRight(NoAuthHeader)
+      cookie <- header.values.toList.find(_.name == Cookie).toRight(NoAuthCookie)
+      token <- crypto.validateSignedToken(cookie.content).toRight(CookieInvalid)
+      username <- Either.catchOnly[NumberFormatException](token).leftMap(_ => CookieInvalid)
     } yield username
 
-     maybeUsername.flatTraverse(retrieveUser.run)
+    maybeUsername.flatTraverse(retrieveUser.run)
   })
 
   val onFailure: AuthedRoutes[String, IO] =
     AuthedRoutes(req => OptionT.liftF(Forbidden(req.authInfo)))
 
-  val middleware = AuthMiddleware(authUser, onFailure)
+  val fetchGuest = userStore.findByName(FallbackUsername).map(_.toRight(UnknownUser(FallbackUsername)))
+
+  val fallbackUser: Kleisli[IO, Either[Error, User], Either[Error, User]] = Kleisli {
+    case Left(NoAuthHeader) => fetchGuest
+    case Left(NoAuthCookie) => fetchGuest
+    case otherwise => IO.pure(otherwise)
+  }
+
+  val mapErrors: Kleisli[IO, Either[Error, User], Either[String, User]] = Kleisli ({
+    case Left(error) => IO.pure(Left(error.getMessage))
+    case Right(user) => IO.pure(Right(user))
+  })
+
+  val authUserWithFallback: Kleisli[IO, Request[IO], Either[String, User]] =
+    authUser andThen fallbackUser andThen mapErrors
+
+  val middleware = AuthMiddleware(authUserWithFallback, onFailure)
 }
+ object AuthenticationService {
+   val Cookie = "authcookie"
+   val FallbackUsername = "guest"
+
+   trait Error extends Throwable
+   object Error {
+     case object NoAuthHeader extends Error { override def getMessage = "No cookies in request header" }
+     case object NoAuthCookie extends Error { override def getMessage = s"No cookie named '$Cookie' found in header"}
+     case object CookieInvalid extends Error { override def getMessage = s"'$Cookie' has invalid signature"}
+     case class UnknownUser(name: String) extends Error { override def getMessage = s"no user '$name' found "}
+   }
+ }
