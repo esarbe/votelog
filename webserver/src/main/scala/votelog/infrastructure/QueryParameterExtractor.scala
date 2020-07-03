1a037bca5665366b86366876a8a4e90bd4e84dee
Raphael Bosshard <raphael.bosshard@gmail.com>
2020-07-03 09:42:08 +0200

    import param

--- webserver/src/main/scala/votelog/infrastructure/QueryParameterExtractor.scala
+++ webserver/src/main/scala/votelog/infrastructure/QueryParameterExtractor.scala
@@ -1,5 +1,7 @@
 package votelog.infrastructure
 
+import votelog.domain.Param
+
 abstract class QueryParameterExtractor[T](implicit ev: Param[T]) {
   def unapply(params: Map[String, Iterable[String]]): Option[T] = ev.decode(params)
 }
