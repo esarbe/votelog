f1e8c13281e5fb26c8ddf9b5f79e0ff51887caab
Raphael Bosshard <raphael.bosshard@gmail.com>
2020-08-30 19:44:27 +0200

    provide gender values

--- core/src/main/scala/votelog/domain/politics/Person.scala
+++ core/src/main/scala/votelog/domain/politics/Person.scala
@@ -14,12 +14,14 @@ case class Person(
 )
 
 object Person {
-  case class Id(value: Int) extends AnyVal { override def toString: String = value.toString}
+  case class Id(value: Int) extends AnyVal { override def toString: String = value.toString }
   case class Name(value: String) extends AnyVal
 
-  sealed trait Gender
+  sealed trait Gender extends Product with Serializable
   object Gender {
     case object Female extends Gender
     case object Male extends Gender
+
+    val values: Set[Gender] = Set(Female, Male)
   }
 }
