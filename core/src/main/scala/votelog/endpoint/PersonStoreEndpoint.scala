ad0604fe36575df5947d8eda436eba500d01c29e
Raphael Bosshard <raphael.bosshard@gmail.com>
2020-08-30 19:44:45 +0200

    implement PersonStoreEndpoint

--- core/src/main/scala/votelog/endpoint/PersonStoreEndpoint.scala
+++ core/src/main/scala/votelog/endpoint/PersonStoreEndpoint.scala
@@ -1,14 +1,23 @@
 package votelog.endpoint
 
-import votelog.domain.crudi.ReadOnlyStoreAlg.IndexQueryParameters
 import votelog.domain.politics
+import votelog.domain.politics.{Context, Language, LegislativePeriod, Person}
+import votelog.orphans.endpoints4s.PersonSchema
 
 trait PersonStoreEndpoint
-  extends ReadOnlyStoreEndpoint {
+  extends ReadOnlyStoreEndpoint
+  with PersonSchema {
 
-  override type Entity = politics.Person
-  override type Id = politics.Person.Id
-  override type Context = politics.Context
-  override type IndexOptions = IndexQueryParameters[Context]
+  override type Entity = Person
+  override type Id = Person.Id
+  override type EntityContext = Language
+  override type IndexContext = Context
 
+  val lp = qs[Int]("lp").xmap(LegislativePeriod.Id.apply)(_.value)
+  val lang = qs[String]("lang").xmap(Language.fromIso639_1Unsafe)(_.iso639_1)
+
+  override val entityContextQuery: QueryString[Language] = lang
+  override val indexContextQuery: QueryString[Context] =
+    (lp & lang)
+      .xmap((politics.Context.apply _).tupled)(c => (c.legislativePeriod, c.language))
 }
